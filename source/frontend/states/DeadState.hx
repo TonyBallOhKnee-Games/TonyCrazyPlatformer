549fa6b26bd4a32d491883999c467e047b796eca
TonyBallOhKnee Games <tonyballohknee@gmail.com>
2024-07-21 17:19:15 -0600

    quick formtat

--- source/frontend/states/DeadState.hx
+++ source/frontend/states/DeadState.hx
@@ -14,13 +14,14 @@ class DeadState extends FlxState
 	{
 		super.create();
 
+		FlxG.sound.music.stop();
 		var dead:FlxSprite = new FlxSprite(0, 0);
 
 		dead.loadGraphic('assets/images/deathScreen/Dead.png', true);
 		dead.frames = FlxAtlasFrames.fromSparrow('assets/images/deathScreen/Dead.png', 'assets/images/deathScreen/Dead.xml');
 		dead.animation.addByPrefix('Dead', 'Dying', 24, false);
-		FlxG.sound.music.stop();
 		FlxG.sound.play("assets/sounds/deadTony.wav", 0.5, false);
+	
 	}
 
 	override function update(elapsed:Float)
