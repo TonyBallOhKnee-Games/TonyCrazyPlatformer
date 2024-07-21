c62559c8ea4fe9d7af58605acf3665933961917b
TonyBallOhKnee Games <tonyballohknee@gmail.com>
2024-07-21 15:07:46 -0600

    le warning update

--- source/frontend/states/WarningState.hx
+++ source/frontend/states/WarningState.hx
@@ -20,7 +20,7 @@ class WarningState extends FlxState
 
 		// add version text
 		var warnText:FlxText = new FlxText(0, 0, 685.6,
-			"This game is still in development. If you encounter any bugs, please report them to the developers on our Discord server.\nPlease be aware that the game may contain themes that are not suitable for all audiences. Art is a form of expression, and we appreciate your understanding and support as we continue to create.\nThank you for your feedback and for being part of our community!",
+			"This game is still in development. If you encounter any bugs, please report them to the developers on our Discord server.\nArt is a form of expression, and we appreciate your understanding and support as we continue to create.\nThank you for your feedback and for being part of our community!",
 			40);
 		warnText.scrollFactor.set();
 		warnText.setFormat("assets/fonts/baloney.ttf", 40, FlxColor.WHITE, CENTER);
