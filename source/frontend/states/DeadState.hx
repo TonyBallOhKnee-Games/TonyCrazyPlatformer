0c513dd77a5735eba561f8eb764b203c29c63e5c
TonyBallOhKnee Games <tonyballohknee@gmail.com>
2024-07-21 17:15:45 -0600

    basic death

--- source/frontend/states/DeadState.hx
+++ source/frontend/states/DeadState.hx
@@ -4,16 +4,22 @@ import flixel.FlxG;
 import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxState;
-import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
-import flxanimate.FlxAnimate;
+import flixel.graphics.frames.FlxAtlasFrames;
 
 class DeadState extends FlxState
 {
 	override public function create():Void
 	{
 		super.create();
+
+		var dead:FlxSprite = new FlxSprite(-221.65, -340.7);
+
+		dead.loadGraphic('assets/images/deathScreen/Dead.png', true);
+		dead.frames = FlxAtlasFrames.fromSparrow('assets/images/deathScreen/Dead.png', 'assets/images/deathScreen/Dead.xml');
+		dead.animation.addByPrefix('Dead', 'Dying', 24, false);
+		FlxG.sound.play("assets/sounds/deadTony.wav", 0.5, true);
 	}
 
 	override function update(elapsed:Float)
