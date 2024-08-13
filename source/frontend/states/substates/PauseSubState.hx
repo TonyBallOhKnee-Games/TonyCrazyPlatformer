19dd81e09627cb89ef828b766a70e281818329a6
TonyBallOhKnee Games <tonyballohknee@gmail.com>
2024-08-13 08:10:49 -0600

    Update PauseSubState.hx

--- source/frontend/states/substates/PauseSubState.hx
+++ source/frontend/states/substates/PauseSubState.hx
@@ -32,7 +32,7 @@ class PauseSubState extends FlxSubState
         fadeOverlay.makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
         fadeOverlay.alpha = 0; // Start transparent
 
-        FlxTween.tween(fadeOverlay, { alpha: 0.7 }, 0.5, 
+        FlxTween.tween(fadeOverlay, { alpha: 0.7 }, 0.8, 
         { 
             type: FlxTween.ONESHOT, 
             ease: FlxEase.quadOut, 
@@ -46,7 +46,7 @@ class PauseSubState extends FlxSubState
         gloria.loadGraphic("assets/images/Pause/gloria.png");
         gloria.antialiasing = true;
         gloria.alpha = 0;
-        FlxTween.tween(gloria, { x: 239, y: 19.2, alpha: 1 }, 1, 
+        FlxTween.tween(gloria, { x: 239, y: 19.2, alpha: 1 }, 0.8, 
         { 
             type: FlxTween.ONESHOT,
             ease: FlxEase.circOut,
@@ -58,7 +58,7 @@ class PauseSubState extends FlxSubState
         tony.loadGraphic("assets/images/Pause/tony.png");
         tony.antialiasing = true;
         tony.alpha = 0;
-        FlxTween.tween(tony, { x: 592.3, y: 168.05, alpha: 1 }, 1, 
+        FlxTween.tween(tony, { x: 592.3, y: 168.05, alpha: 1 }, 0.8, 
         { 
             type: FlxTween.ONESHOT,
             ease: FlxEase.circOut,
@@ -70,7 +70,7 @@ class PauseSubState extends FlxSubState
         baloney.loadGraphic("assets/images/Pause/baloney.png");
         baloney.antialiasing = true;
         baloney.alpha = 0;
-        FlxTween.tween(baloney, { x: -34.65, y: 126.5, alpha: 1 }, 1, 
+        FlxTween.tween(baloney, { x: -34.65, y: 126.5, alpha: 1 }, 0.8, 
         { 
             type: FlxTween.ONESHOT,
             ease: FlxEase.circOut,
@@ -83,7 +83,7 @@ class PauseSubState extends FlxSubState
         pauseText.loadGraphic("assets/images/Pause/paused.png");
         pauseText.antialiasing = true;
         pauseText.alpha = 0;
-        FlxTween.tween(pauseText, { x: 396.55, y: 543.95, alpha: 1 }, 1, 
+        FlxTween.tween(pauseText, { x: 396.55, y: 543.95, alpha: 1 }, 0.8, 
         { 
             type: FlxTween.ONESHOT,
             ease: FlxEase.circOut,
@@ -91,77 +91,101 @@ class PauseSubState extends FlxSubState
         });
         add(pauseText);
 
-        title = new FlxText(0, 0, 0, "Press Q to quit. \n Press ESC to unpause");
-        title.setFormat(null, 32, FlxColor.WHITE, "center");
+        title = new FlxText(0, -200, 0, "Press Q to quit. \n Press ESC to unpause");
+        title.setFormat("assets/fonts/baloney.ttf", 32, FlxColor.WHITE, "center");
+
+        FlxTween.tween(title, { x: 0, y: 0 }, 0.8, 
+            { 
+                type: FlxTween.ONESHOT,
+                ease: FlxEase.circOut,
+                startDelay: 0
+            });
+
         add(title);
 
     }
 
     private function startFadeOut():Void
     {
-        FlxTween.tween(fadeOverlay, { alpha: 0 }, 0.5, 
+        FlxTween.tween(fadeOverlay, { alpha: 0 }, 0.8, 
         { 
             type: FlxTween.ONESHOT, 
             ease: FlxEase.quadOut, 
             onComplete: resumeGame 
         });
 
-        FlxTween.tween(pauseText, { x: 396.55, y: -144.55, alpha: 0 }, 0.5, 
+        FlxTween.tween(pauseText, { x: 396.55, y: -144.55, alpha: 0 }, 0.8, 
         { 
             type: FlxTween.ONESHOT,
             ease: FlxEase.circIn
         });
 
-        FlxTween.tween(baloney, { x: -697.65, y: 126.5, alpha: 0 }, 0.5, 
+        FlxTween.tween(baloney, { x: -697.65, y: 126.5, alpha: 0 }, 0.8, 
         { 
             type: FlxTween.ONESHOT,
             ease: FlxEase.circIn
         });
 
-        FlxTween.tween(tony, { x: 1323.3, y: 168.05, alpha: 0 }, 0.5, 
+        FlxTween.tween(tony, { x: 1323.3, y: 168.05, alpha: 0 }, 0.8, 
         { 
             type: FlxTween.ONESHOT,
             ease: FlxEase.circIn
         });
 
-        FlxTween.tween(gloria, { x: 239, y: 784.2, alpha: 0 }, 0.5, 
+        FlxTween.tween(gloria, { x: 239, y: 784.2, alpha: 0 }, 0.8, 
         { 
             type: FlxTween.ONESHOT,
             ease: FlxEase.circIn
         });
+
+        FlxTween.tween(title, { x: 0, y: -200 }, 0.8, 
+        { 
+            type: FlxTween.ONESHOT,
+            ease: FlxEase.circIn,
+            startDelay: 0
+        });
+
+        add(title);
     }
 
     private function startFadeOutQuit():Void
         {
-            FlxTween.tween(fadeOverlay, { alpha: 1 }, 0.5, 
+            FlxTween.tween(fadeOverlay, { alpha: 1 }, 0.8, 
             { 
                 type: FlxTween.ONESHOT, 
                 ease: FlxEase.quadOut, 
                 onComplete: quittingTime 
             });
     
-            FlxTween.tween(pauseText, { x: 396.55, y: -144.55, alpha: 0 }, 0.5, 
+            FlxTween.tween(pauseText, { x: 396.55, y: -144.55, alpha: 0 }, 0.8, 
             { 
                 type: FlxTween.ONESHOT,
                 ease: FlxEase.circIn
             });
     
-            FlxTween.tween(baloney, { x: -697.65, y: 126.5, alpha: 0 }, 0.5, 
+            FlxTween.tween(baloney, { x: -697.65, y: 126.5, alpha: 0 }, 0.8, 
             { 
                 type: FlxTween.ONESHOT,
                 ease: FlxEase.circIn
             });
     
-            FlxTween.tween(tony, { x: 1323.3, y: 168.05, alpha: 0 }, 0.5, 
+            FlxTween.tween(tony, { x: 1323.3, y: 168.05, alpha: 0 }, 0.8, 
             { 
                 type: FlxTween.ONESHOT,
                 ease: FlxEase.circIn
             });
     
-            FlxTween.tween(gloria, { x: 239, y: 784.2, alpha: 0 }, 0.5, 
+            FlxTween.tween(gloria, { x: 239, y: 784.2, alpha: 0 }, 0.8, 
             { 
                 type: FlxTween.ONESHOT,
                 ease: FlxEase.circIn
+            });
+            
+            FlxTween.tween(title, { x: 0, y: -200 }, 0.8, 
+            { 
+                type: FlxTween.ONESHOT,
+                ease: FlxEase.circIn,
+                startDelay: 0
             });
         }
 
