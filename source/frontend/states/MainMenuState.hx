446b018a7b2f6db74916833a2ff35420973e0482
fgdg <zsolardevofficial@gmail.com>
2024-08-15 19:07:21 -0400

    Fix Tweens

--- source/frontend/states/MainMenuState.hx
+++ source/frontend/states/MainMenuState.hx
@@ -4,36 +4,36 @@ import flixel.FlxG;
 import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
 import flixel.ui.FlxButton;
 import flixel.util.FlxColor;
 import openfl.Lib;
 import openfl.net.URLRequest;
-import flixel.graphics.frames.FlxAtlasFrames;
-import flixel.tweens.FlxTween;
-import flixel.tweens.FlxEase;
-
 
 class MainMenuState extends FlxState
 {
 	public static var curSelected:Int = 0; // Selected menu option (0: Start, 1: Load, 2: Options)
 
 	private var selectorguy:FlxSprite;
+
 	public var logoShit:FlxSprite = new FlxSprite(-612.65, -59.2);
 	public var tonyDovark:FlxSprite = new FlxSprite(986.9, 347.45);
 
 	var tween:FlxTween;
+	var canChangeSelection:Bool = false;
+
 	override public function create():Void
 	{
 		super.create();
 
 		curSelected = 0; // Set curSelected to 0 at the start of the state
 
-
-		if (!FlxG.sound.music.playing) {
-
+		if (!FlxG.sound.music.playing)
+		{
 			FlxG.sound.playMusic("assets/music/MainNew.wav", 0.5, true);
-
 		}
 
 		// Add background image
@@ -41,8 +41,6 @@ class MainMenuState extends FlxState
 		sky.loadGraphic("assets/images/title/sky.png");
 		sky.antialiasing = true;
 		add(sky);
-
-		
 
 		logoShit.loadGraphic('assets/images/title/logo.png', true);
 		logoShit.frames = FlxAtlasFrames.fromSparrow('assets/images/title/logo.png', 'assets/images/title/logo.xml');
@@ -61,56 +59,44 @@ class MainMenuState extends FlxState
 		var start:FlxSprite = new FlxSprite(1430.6, 465.35);
 		start.loadGraphic("assets/images/title/new game.png");
 		start.antialiasing = true;
-		
-		FlxTween.tween(start, { x: 374.95, y: 465.35 }, 2, 
-			{ 
-				type:       ONESHOT,
-				ease:       FlxEase.circInOut,
-				startDelay: 0.83333333333333333333333333333333
-			}
-		);
+
+		FlxTween.tween(start, {x: 374.95, y: 465.35}, 2, {
+			ease: FlxEase.circInOut,
+			startDelay: 0.83333333333333333333333333333333
+		});
 
 		add(start);
 
 		var load:FlxSprite = new FlxSprite(1424.4, 523.25);
 		load.loadGraphic("assets/images/title/loadgame.png");
 		load.antialiasing = true;
-		
-		FlxTween.tween(load, { x: 493.75, y: 523.25 }, 2, 
-			{ 
-				type:       ONESHOT,
-				ease:       FlxEase.circInOut,
-				startDelay: 0.83333333333333333333333333333333
-			}
-		);
-		
+
+		FlxTween.tween(load, {x: 493.75, y: 523.25}, 2, {
+			ease: FlxEase.circInOut,
+			startDelay: 0.83333333333333333333333333333333
+		});
+
 		add(load);
 
 		var options:FlxSprite = new FlxSprite(1456.3, 581.15);
 		options.loadGraphic("assets/images/title/options.png");
 		options.antialiasing = true;
-		
-		FlxTween.tween(options, { x: 637, y: 581.15 }, 2, 
-			{ 
-				type:       ONESHOT,
-				ease:       FlxEase.circInOut,
-				startDelay: 0.83333333333333333333333333333333
-			}
-		);
-		
+
+		FlxTween.tween(options, {x: 637, y: 581.15}, 2, {
+			ease: FlxEase.circInOut,
+			startDelay: 0.83333333333333333333333333333333
+		});
+
 		add(options);
 
 		var credtis:FlxSprite = new FlxSprite(1462.45, 639.05);
 		credtis.loadGraphic("assets/images/title/credits.png");
 		credtis.antialiasing = true;
-		
-		FlxTween.tween(credtis, { x: 709.75, y: 639.05 }, 2, 
-			{ 
-				type:       ONESHOT,
-				ease:       FlxEase.circInOut,
-				startDelay: 0.83333333333333333333333333333333
-			}
-		);
+
+		FlxTween.tween(credtis, {x: 709.75, y: 639.05}, 2, {
+			ease: FlxEase.circInOut,
+			startDelay: 0.83333333333333333333333333333333
+		});
 
 		add(credtis);
 
@@ -118,13 +104,14 @@ class MainMenuState extends FlxState
 		selectorguy.loadGraphic("assets/images/title/unused/selectorthingy.png");
 		selectorguy.antialiasing = true;
 
-		FlxTween.tween(selectorguy, { x: 577.65, y: 465.35 }, 2, 
-			{ 
-				type:       ONESHOT,
-				ease:       FlxEase.quartOut,
-				startDelay: 2
+		FlxTween.tween(selectorguy, {x: 577.65, y: 465.35}, 2, {
+			ease: FlxEase.quartOut,
+			startDelay: 2,
+			onComplete: function(t:FlxTween)
+			{
+				canChangeSelection = true;
 			}
-			);
+		});
 		add(selectorguy);
 
 		// Add version text
@@ -132,16 +119,12 @@ class MainMenuState extends FlxState
 		gameVer.scrollFactor.set();
 		gameVer.setFormat("assets/fonts/baloney.ttf", 40, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		gameVer.antialiasing = true;
-		
-		FlxTween.tween(gameVer, { x: 12, y: 12 }, 2, 
-			{ 
-				type:       ONESHOT,
-				ease:       FlxEase.circInOut,
-				startDelay: 1.3
 
-			}
-		);
-		
+		FlxTween.tween(gameVer, {x: 12, y: 12}, 2, {
+			ease: FlxEase.circInOut,
+			startDelay: 1.3
+		});
+
 		add(gameVer);
 
 		// Add YouTube button
@@ -160,9 +143,9 @@ class MainMenuState extends FlxState
 	}
 
 	private function goToCredits():Void
-		{
-			FlxG.switchState(new CreditsState());
-		}
+	{
+		FlxG.switchState(new CreditsState());
+	}
 
 	private function openYouTube():Void
 	{
@@ -180,57 +163,48 @@ class MainMenuState extends FlxState
 		switch (curSelected)
 		{
 			case 0:
-			//selectorguy.x = 577.65;	
-			//selectorguy.y = 465.35;
+				// selectorguy.x = 577.65;
+				// selectorguy.y = 465.35;
 
-			tween = FlxTween.tween(selectorguy, { x: 577.65, y: 465.35 }, 0.2, 
-				{ 
-					type:       ONESHOT,
-					ease:       FlxEase.quadOut,
-				}
-			);
+				tween = FlxTween.tween(selectorguy, {x: 577.65, y: 465.35}, 0.1, {
+					ease: FlxEase.circOut,
+				});
 			case 1:
-			//selectorguy.x = 715.85;	
-			//selectorguy.y = 529.15;
+				// selectorguy.x = 715.85;
+				// selectorguy.y = 529.15;
 
-			tween = FlxTween.tween(selectorguy, { x: 715.85, y: 529.15 }, 0.2, 
-				{ 
-					type:       ONESHOT,
-					ease:       FlxEase.quadOut,
-				}
-			);
+				tween = FlxTween.tween(selectorguy, {x: 715.85, y: 529.15}, 0.1, {
+					ease: FlxEase.circOut,
+				});
 			case 2:
-			//selectorguy.x = 790.1;
-			//selectorguy.y = 581.15;
+				// selectorguy.x = 790.1;
+				// selectorguy.y = 581.15;
 
-			tween = FlxTween.tween(selectorguy, { x: 790.1, y: 581.15 }, 0.2, 
-				{ 
-					type:       ONESHOT,
-					ease:       FlxEase.quadOut,
-				}
-			);
+				tween = FlxTween.tween(selectorguy, {x: 790.1, y: 581.15}, 0.1, {
+					ease: FlxEase.circOut,
+				});
 			case 3:
-			//selectorguy.x = 851.3;
-			//selectorguy.y = 639.05;	
-			
-			tween = FlxTween.tween(selectorguy, { x: 851.3, y: 639.05 }, 0.2, 
-				{ 
-					type:       ONESHOT,
-					ease:       FlxEase.quadOut,
-				}
-			);
+				// selectorguy.x = 851.3;
+				// selectorguy.y = 639.05;
+
+				tween = FlxTween.tween(selectorguy, {x: 851.3, y: 639.05}, 0.1, {
+					ease: FlxEase.circOut,
+				});
 		}
 	}
 
 	override function update(elapsed:Float):Void
 	{
-		if (FlxG.keys.justPressed.UP || FlxG.keys.justPressed.W)
+		if (canChangeSelection)
 		{
-			changeItem(-1);
-		}
-		if (FlxG.keys.justPressed.DOWN || FlxG.keys.justPressed.S)
-		{
-			changeItem(1);
+			if (FlxG.keys.justPressed.UP || FlxG.keys.justPressed.W)
+			{
+				changeItem(-1);
+			}
+			if (FlxG.keys.justPressed.DOWN || FlxG.keys.justPressed.S)
+			{
+				changeItem(1);
+			}
 		}
 		if (FlxG.keys.justPressed.ENTER)
 		{
@@ -249,6 +223,4 @@ class MainMenuState extends FlxState
 
 		super.update(elapsed);
 	}
-
-
 }
