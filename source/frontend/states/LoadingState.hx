8ad6f8c16e22e5a5d5982ff76a3ffc09de7b5893
fgdg <zsolardevofficial@gmail.com>
2024-08-24 12:52:16 -0400

    Loading Screen Start!

--- source/frontend/states/LoadingState.hx
+++ source/frontend/states/LoadingState.hx
@@ -21,51 +21,15 @@ class LoadingState extends FlxTransitionableState
 		// Set background color
 		bgColor = FlxColor.BLACK;
 
-		// Create and add loading text
-		loadingText = new FlxText(0, FlxG.height / 2 - 10, FlxG.width, "Loading...");
-		loadingText.setFormat(null, 16, FlxColor.WHITE, "center");
-		add(loadingText);
-
-		// Create and add loading bar
 		loadingBar = new FlxSprite(FlxG.width / 4, FlxG.height / 2 + 20);
-		loadingBar.makeGraphic(FlxG.width / 2, 10, FlxColor.WHITE);
+		loadingBar.makeGraphic(Std.int(FlxG.width / 2), 10, FlxColor.WHITE);
 		loadingBar.scrollFactor.set();
 		loadingBar.scale.x = 0;
 		add(loadingBar);
-
-		// Start loading assets
-		loadAssets();
-	}
-
-	private function loadAssets():Void
-	{
-		// Load images
-		FlxG.assets.loadGraphic("assets/images/someImage.png", false, true, 100, 100);
-		FlxG.assets.loadGraphic("assets/images/anotherImage.png", false, true, 100, 100);
-
-		// Load sounds
-		FlxG.assets.loadSound("assets/sounds/someSound.mp3");
-		FlxG.assets.loadSound("assets/sounds/anotherSound.mp3");
-
-		// Preload the assets
-		FlxG.assets.onLoadComplete = onAssetsLoaded;
-	}
-
-	private function onAssetsLoaded():Void
-	{
-		// Transition to the next state when loading is complete
-		FlxG.switchState(new PlayState());
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
-
-		// Update loading progress
-		loadingProgress = FlxG.assets.progress;
-		loadingBar.scale.x = loadingProgress;
-
-		// Optionally, you can display the percentage of loading
-		loadingText.text = "Loading... " + Math.round(loadingProgress * 100) + "%";
 	}
 }
